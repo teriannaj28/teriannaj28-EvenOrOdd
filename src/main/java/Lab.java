dffa533d78c147b503e798eb594eee19cdec0573
teriannaj28 <teriannaj@gmail.com>
2023-02-24 21:00:55 +0000

    Coding Challenge: Even or Odd (Week 2: Day 5)

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,16 @@ public class Lab {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        return "";
+        String result = "";
+
+        if ((n % 2) == 0) {
+            result = "Even";
+        } else if ((n % 2) == 1) {
+            result = "Odd";
+        } else if ((n % 2) == -1) {
+            result = "Odd";
+        }
+        
+        return result;
     }
 }
